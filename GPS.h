0b66cbefdd4a8bc02f864868ae6dddd1083e7ac3
John Duggins <jduggins@jewel.local>
2010-08-17 20:30:16 -0500

    Adding GPS subapp

---
+++ GPS.h
@@ -0,0 +1,16 @@
+//
+//  GPS.h
+//  JCD Portfolio
+//
+//  Created by John Duggins on 8/17/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GPS : PrimaryViewController {
+
+}
+
+@end
---
+++ GPS.m
@@ -0,0 +1,14 @@
+//
+//  GPS.m
+//  JCD Portfolio
+//
+//  Created by John Duggins on 8/17/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "GPS.h"
+
+
+@implementation GPS
+
+@end
